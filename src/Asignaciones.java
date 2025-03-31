acdd7ddc3c42ac461d307e15c15a8d3d0565a0cb
Alvaro Burgos <alvarobda11@gmail.com>
2025-03-31 13:10:13 +0200

    Avance en clase Reparacion y Empleado

--- src/Asignaciones.java
+++ src/Asignaciones.java
@@ -1,5 +1,37 @@
 public class Asignaciones {
 
     int idAsignacion;
+    int idEmpleado;
+    int idReparacion;
+
+    public Asignaciones (int idAsignacion, int idEmpleado, int idReparacion) {
+        this.idAsignacion = idAsignacion;
+        this.idEmpleado = idEmpleado;
+        this.idReparacion = idReparacion;
+    }
+
+    public int getIdAsignacion() {
+        return idAsignacion;
+    }
+
+    public void setIdAsignacion(int idAsignacion) {
+        this.idAsignacion = idAsignacion;
+    }
+
+    public int getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public void setIdEmpleado(int idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public int getIdReparacion() {
+        return idReparacion;
+    }
+
+    public void setIdReparacion(int idReparacion) {
+        this.idReparacion = idReparacion;
+    }
 
 }
--- src/TALLER.java
+++ src/TALLER.java
@@ -1,16 +1,10 @@
 // CLASE CONTROLADOR
+import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
-public class TALLER {
 
-    List<Cliente> clientes = new ArrayList<>();
-    List<Vehiculo> vehiculos = new ArrayList<>();
-    List<Reparacion> reparaciones = new ArrayList<>();
-    List<Empleado> empleados = new ArrayList<>();
+public class Taller {
+    
 
-
-    public static void main(String[]args) {
-        
-    }
 
 }
