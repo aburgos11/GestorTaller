c407e2b51d124b97c07f8857f247cc0836b04550
Alvaro Burgos <alvarobda11@gmail.com>
2025-03-31 12:01:54 +0200

    Avance en clase

--- src/Reparacion.java
+++ src/Reparacion.java
@@ -15,5 +15,45 @@ public class Reparacion {
         
     }
 
+    public int getIdReparacion() {
+        return idReparacion;
+    }
+
+    public void setIdReparacion(int idReparacion) {
+        this.idReparacion = idReparacion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getMatriculaVehiculo() {
+        return matriculaVehiculo;
+    }
+
+    public void setMatriculaVehiculo(String matriculaVehiculo) {
+        this.matriculaVehiculo = matriculaVehiculo;
+    }
+
+    public int getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public void setIdEmpleado(int idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
 
 }
