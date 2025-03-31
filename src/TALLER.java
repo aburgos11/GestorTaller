eb04221b05adbd9d6052abec6d6d847d16bc3517
Alvaro Burgos <alvarobda11@gmail.com>
2025-03-31 12:41:16 +0200

    Avance en clase Reparacion y Empleado

--- src/TALLER.java
+++ src/TALLER.java
@@ -1,10 +1,16 @@
+// CLASE CONTROLADOR
 import java.util.ArrayList;
 import java.util.List;
-
 public class TALLER {
 
     List<Cliente> clientes = new ArrayList<>();
     List<Vehiculo> vehiculos = new ArrayList<>();
     List<Reparacion> reparaciones = new ArrayList<>();
+    List<Empleado> empleados = new ArrayList<>();
+
+
+    public static void main(String[]args) {
+        
+    }
 
 }
