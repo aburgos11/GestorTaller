757f96e4e9a281d554379e0e4437eaad7aa30b7c
Álvaro Burgos <alvarobda11@gmail.com>
2025-04-02 10:20:43 +0200

    Trabajo desde portatil

--- src/model/ConexionBD.java
+++ src/model/ConexionBD.java
@@ -1,9 +1,11 @@
+package model;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 public class ConexionBD {
  // URL de conexión a la base de datos MySQL
-    private static final String URL = "jdbc:mysql://localhost:3306/Taller"; // Cambia el nombre de la base de datos
+    private static final String URL = "jdbc:mysql://localhost:3306/gestor_taller"; // Cambia el nombre de la base de datos
     private static final String USUARIO = "user"; // Nombre de usuario de MySQL
     private static final String CONTRASENA = "user"; // Contraseña del usuario de MySQL
     // Método para establecer la conexión con la base de datos
--- src/model/Reparacion.java
+++ src/model/Reparacion.java
@@ -1,3 +1,5 @@
+package model;
+
 public class Reparacion {
 
     int idReparacion;
