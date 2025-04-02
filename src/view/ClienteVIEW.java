ceb0ad3a6455bff8322d4864e300ef37e0e40d3a
Álvaro Burgos <alvarobda11@gmail.com>
2025-04-02 08:37:12 +0200

    Trabajo desde casa actualizado

--- src/view/ClienteVIEW.java
+++ src/view/ClienteVIEW.java
@@ -8,55 +8,40 @@ public class ClienteVIEW {
     private Scanner sc = new Scanner(System.in);
     private ClienteDAO clienteDAO = new ClienteDAO();
 
-    public static void main(String[] args) {
+    public void menuClientes() {
         Scanner scanner = new Scanner(System.in);
         int opcionClientes;
         do {
             System.out.println("¿Qué acción deseas realizar?");
-            System.out.println("1. Buscar cliente");
-            System.out.println("2. Agregar cliente");
-            System.out.println("3. Modificar cliente");
-            System.out.println("4. Eliminar cliente");
+            System.out.println("1. Ver lista de clientes");
+            System.out.println("2. Buscar cliente");
+            System.out.println("3. Agregar cliente");
+            System.out.println("4. Modificar cliente");
+            System.out.println("5. Eliminar cliente");
             System.out.println("0. Volver al menú inicial");
             opcionClientes = scanner.nextInt();
             scanner.nextLine();
     
             switch (opcionClientes) {
-                case 1: // Buscar cliente
-                System.out.println("Introduce el ID del cliente que deseas buscar");
+                case 1: // Ver lista de clientes
+                listarClientes();
+                break;
+
+                case 2: // Buscar cliente
+                buscarCliente();                                     
+                break;
+     
+                case 3: // Agregar cliente
+                agregarCliente();
                 break;
     
-                case 2: // Agregar cliente
-                System.out.println("Introduce los datos del nuevo cliente:");
-    
-                System.out.println("Ingresa el ID");
-                int idCliente = scanner.nextInt();
-                scanner.nextLine();
-    
-                System.out.println("Ingresa el nombre");
-                String nombreCliente = scanner.nextLine();
-                scanner.nextLine();
-    
-                System.out.println("Ingresa el número de teléfono");
-                String telefonoCliente = scanner.nextLine();
-                scanner.nextLine();
-    
-                System.out.println("Ingresa el email");
-                String emailCliente = scanner.nextLine();
-                scanner.nextLine();
-                                                
+                case 4: // Modificar cliente
+                modificarCliente();       
                 break;
-    
-    
-                case 3: // Modificar cliente
-                            
+
+                case 5: // Eliminar cliente
+                eliminarCliente();
                 break;
-    
-    
-                case 4: // Eliminar cliente
-                           
-                break;
-    
     
                 case 0: System.out.println("Volviendo al menú inicial");
                 break;
@@ -65,10 +50,7 @@ public class ClienteVIEW {
                                                         
     
     
-        }
+        } while (opcionClientes != 0);
 
     }
 }    
-    
-    
-
